504b6809842a2dbaa79ce0913530234416752a66
Filip Kojic <filip.kojic@logeecom.com>
2024-08-21 00:08:03 +0200

    Create dom helper
    ISSUE: MIDU-926

--- src/Application/Presentation/Public/js/main.js
+++ src/Application/Presentation/Public/js/main.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const contentDiv = document.getElementById('content');
     const router = new Router(contentDiv);
 
-    window.removeCssFile = removeCssFile;
-    window.loadCssFile = loadCssFile;
-    window.createElement = createElement;
+    window.removeCssFile = DomHelper.removeCssFile;
+    window.loadCssFile = DomHelper.loadCssFile;
+    window.createElement = DomHelper.createElement;
 
 // Register routes with the router
     router.registerRoute('/admin', loadDashboard);
@@ -25,52 +25,4 @@ document.addEventListener('DOMContentLoaded', () => {
             router.navigate(`/${sectionId}`);
         });
     });
-
-
-    /**
-     * Function to create an HTML element
-     * @param {string} tag - The HTML tag to create (e.g., 'div', 'span', 'input')
-     * @param {object} attributes - The attributes to add to the element (e.g., { id: 'myId', class: 'myClass' })
-     * @param {string} textContent - The text content to set inside the element (optional)
-     * @returns {HTMLElement} - The created HTML element
-     */
-    function createElement(tag, attributes = {}, textContent = '') {
-        const element = document.createElement(tag);
-        for (const [key, value] of Object.entries(attributes)) {
-            element.setAttribute(key, value);
-        }
-        if (textContent) {
-            element.textContent = textContent;
-        }
-        return element;
-    }
-
-    /**
-     * Loads a CSS file dynamically into the document's head.
-     * If the CSS file is already loaded, it won't load it again.
-     *
-     * @param {string} filename - The path to the CSS file to load.
-     */
-    function loadCssFile(filename) {
-        const existingLink = document.querySelector(`link[href="${filename}"]`);
-        if (!existingLink) {
-            const link = document.createElement('link');
-            link.rel = 'stylesheet';
-            link.href = filename;
-            document.head.appendChild(link);
-        }
-    }
-
-    /**
-     * Removes a CSS file dynamically from the document's head.
-     * If the CSS file is not found, it does nothing.
-     *
-     * @param {string} filename - The path to the CSS file to remove.
-     */
-    function removeCssFile(filename) {
-        const existingLink = document.querySelector(`link[href="${filename}"]`);
-        if (existingLink) {
-            existingLink.remove();
-        }
-    }
 });
