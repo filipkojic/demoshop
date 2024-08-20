75dbe4a49b83dc9ab0a9975a9005ad39d93b1247
Filip Kojic <filip.kojic@logeecom.com>
2024-08-21 00:14:20 +0200

    Change dashboard to use dom helper
    ISSUE: MIDU-926

--- src/Application/Presentation/Public/js/dashboard.js
+++ src/Application/Presentation/Public/js/dashboard.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     window.loadDashboard = loadDashboard;
 
     function createProductCountDiv(stats) {
-        const productCountDiv = createElement('div');
-        const productCountLabel = createElement('label', {for: 'productCount'}, 'Products count:');
-        const productCountInput = createElement('input', {
+        const productCountDiv = DomHelper.createElement('div');
+        const productCountLabel = DomHelper.createElement('label', {for: 'productCount'}, 'Products count:');
+        const productCountInput = DomHelper.createElement('input', {
             type: 'text',
             id: 'productCount',
             value: stats.productCount || '0',
@@ -17,9 +17,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function createCategoryCountDiv(stats) {
-        const categoryCountDiv = createElement('div');
-        const categoryCountLabel = createElement('label', {for: 'categoryCount'}, 'Categories count:');
-        const categoryCountInput = createElement('input', {
+        const categoryCountDiv = DomHelper.createElement('div');
+        const categoryCountLabel = DomHelper.createElement('label', {for: 'categoryCount'}, 'Categories count:');
+        const categoryCountInput = DomHelper.createElement('input', {
             type: 'text',
             id: 'categoryCount',
             value: stats.categoryCount || '0',
@@ -31,9 +31,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function createHomepageCountDiv(stats) {
-        const homepageCountDiv = createElement('div');
-        const homepageCountLabel = createElement('label', {for: 'homepageCount'}, 'Home page opening count:');
-        const homepageCountInput = createElement('input', {
+        const homepageCountDiv = DomHelper.createElement('div');
+        const homepageCountLabel = DomHelper.createElement('label', {for: 'homepageCount'}, 'Home page opening count:');
+        const homepageCountInput = DomHelper.createElement('input', {
             type: 'text',
             id: 'homepageCount',
             value: stats.homeViewCount || '0',
@@ -45,9 +45,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function createMostViewedProductDiv(stats) {
-        const mostViewedProductDiv = createElement('div');
-        const mostViewedProductLabel = createElement('label', {for: 'mostViewedProduct'}, 'Most often viewed product:');
-        const mostViewedProductInput = createElement('input', {
+        const mostViewedProductDiv = DomHelper.createElement('div');
+        const mostViewedProductLabel = DomHelper.createElement('label', {for: 'mostViewedProduct'}, 'Most often viewed product:');
+        const mostViewedProductInput = DomHelper.createElement('input', {
             type: 'text',
             id: 'mostViewedProduct',
             value: stats.mostViewedProduct ? stats.mostViewedProduct.title : 'N/A',
@@ -59,9 +59,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function createProductViewsDiv(stats) {
-        const productViewsDiv = createElement('div');
-        const productViewsLabel = createElement('label', {for: 'productViews'}, 'Number of most viewed product views:');
-        const productViewsInput = createElement('input', {
+        const productViewsDiv = DomHelper.createElement('div');
+        const productViewsLabel = DomHelper.createElement('label', {for: 'productViews'}, 'Number of most viewed product views:');
+        const productViewsInput = DomHelper.createElement('input', {
             type: 'text',
             id: 'productViews',
             value: stats.mostViewedProduct ? stats.mostViewedProduct.viewCount : '0',
@@ -76,20 +76,20 @@ document.addEventListener('DOMContentLoaded', () => {
      * Function to load the Dashboard page content
      */
     async function loadDashboard() {
-        removeCssFile('/src/Application/Presentation/Public/css/categories.css');
-        loadCssFile('/src/Application/Presentation/Public/css/dashboard.css');
+        DomHelper.removeCssFile('/src/Application/Presentation/Public/css/categories.css');
+        DomHelper.loadCssFile('/src/Application/Presentation/Public/css/dashboard.css');
         contentDiv.innerHTML = '';
 
         const stats = await AjaxService.get('/getStatistics');
 
-        const dashboardGrid = createElement('div', {class: 'dashboardGrid'});
+        const dashboardGrid = DomHelper.createElement('div', {class: 'dashboardGrid'});
 
         const productCountDiv = createProductCountDiv(stats);
         const categoryCountDiv = createCategoryCountDiv(stats);
         const homepageCountDiv = createHomepageCountDiv(stats);
         const mostViewedProductDiv = createMostViewedProductDiv(stats);
         const productViewsDiv = createProductViewsDiv(stats);
-        const spacingDiv = createElement('div');
+        const spacingDiv = DomHelper.createElement('div');
 
         dashboardGrid.appendChild(productCountDiv);
         dashboardGrid.appendChild(homepageCountDiv);
